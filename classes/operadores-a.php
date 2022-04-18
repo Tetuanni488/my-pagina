bd12709ff1ca9c64d1d9747f968ea9b5a794854d
SolaymanCasa <solaymanad@outlook.com>
2022-04-19 00:58:29 +0200

    first commit

---
+++ classes/operadores-a.php
@@ -0,0 +1,18 @@
+<?php 
+
+$a = 2;
+$b = 4;
+
+//Modulo
+
+$modulo = $a%$b;
+
+echo "modulo $modulo \n";
+
+//Exponenciación
+
+$exponenciacion = $a**$b;
+
+echo "Exponenciación $exponenciacion \n";
+
+?>
